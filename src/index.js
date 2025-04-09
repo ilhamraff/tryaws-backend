e7c4a21f69ae69bcb204c086c1023c4341172ea1
ilhamraff <ifadhilah261@gmail.com>
2025-04-09 14:58:14 +0700

    fix bugs host value

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running at http://0.0.0.0:${port}`);
 });
