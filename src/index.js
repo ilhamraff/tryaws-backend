420345a91606e217db472dbdd5539955a445f2eb
ilhamraff <ifadhilah261@gmail.com>
2025-04-09 20:16:26 +0700

    handle cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-
 const port = 5000;
+
+app.use(cors());
 
 const users = [
   { id: 1, name: "Ilham", email: "ilham@example.com" },
